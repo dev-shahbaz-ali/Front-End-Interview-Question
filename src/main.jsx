3a8af351e1596e246197357707d22bf35fae6416
dev-shahbaz-ali <devshahbazali.it@gmail.com>
2025-10-08 10:38:25 +0500

    Frontend-question-Updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ThemeContextProvider } from "./components/theme-provider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeContextProvider defaultTheme="light">
-      <App />
-    </ThemeContextProvider>
+    <App />
   </React.StrictMode>
 );
